5b7055351e1b6300cdddf17cccd3a119a63bdfa6
ShakhzodUsmanov <shaxzodusmanov11@gmail.com>
2023-11-30 01:03:52 +0500

    first commit

---
+++ quiz_data.py
@@ -0,0 +1,47 @@
+quiz_data = [
+    {
+        'question': 'What is Python?',
+        'choices': ['A snake species', 'A programming language', 'A type of software', 'A machine learning algorithm'],
+        'answer': 'A programming language'
+    },
+    {
+        'question': 'Which of the following statements is used for comments in Python?',
+        'choices': ['#', '/* */', '//', 'REM'],
+        'answer': '#'
+    },
+    {
+        'question': 'What is the purpose of the "if" statement in Python?',
+        'choices': ['To declare a variable', 'To create a loop', 'To make decisions', 'To define a function'],
+        'answer': 'To make decisions'
+    },
+    {
+        'question': 'In Python, how do you define a function?',
+        'choices': ['def function_name()', 'function function_name()', 'define function_name()', 'func function_name()'],
+        'answer': 'def function_name()'
+    },
+    {
+        'question': 'What does the term "PEP" stand for in Python?',
+        'choices': ['Python Enhancement Proposal', 'Programming Efficiency Protocol', 'Python Execution Plan', 'Programming Error Prevention'],
+        'answer': 'Python Enhancement Proposal'
+    },
+    {
+        'question': 'How do you open a file in Python?',
+        'choices': ['fopen()', 'open()', 'read_file()', 'file_open()'],
+        'answer': 'open()'
+    },
+    {
+        'question': 'What is the purpose of the "elif" keyword in Python?',
+        'choices': ['It is a typo, meant to be "else if"', 'To terminate a loop', 'To handle multiple conditions', 'It stands for "element if"'],
+        'answer': 'To handle multiple conditions'
+    },
+    {
+        'question': 'Which data type is mutable in Python?',
+        'choices': ['String', 'Tuple', 'List', 'Set'],
+        'answer': 'List'
+    },
+    {
+        'question': 'What is the purpose of the "try" and "except" blocks in Python?',
+        'choices': ['To define a function', 'To create a loop', 'To handle exceptions and errors', 'To declare a variable'],
+        'answer': 'To handle exceptions and errors'
+    }
+]
